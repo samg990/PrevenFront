e30eb0d65e4395216e3f621cfbada41ecf27a5ec
Sam <sam861g@gmail.com>
2020-06-08 13:21:03 -0700

    watchdog live file monitoring

--- temptext.py
+++ temptext.py
@@ -1,18 +1,48 @@
 import numpy as np
 import cv2
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
-last_t = None
 
-def lasttemp():
-    
-    with open('sensortemp.txt') as f:
-        for line in f:
-            pass
-        global last_line
-        last_line = line
+class MyHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        last_t = None
+
+        def lasttemp():
+            
+            with open('sensortemp.txt') as f:
+                for line in f:
+                    pass
+                
+                last_line = line
+                
+                
+                return last_line
+            
+        last_t = lasttemp()
+
+        print(last_t)
         
         
-        return last_line
-    
-last_t = lasttemp()
+        
+        
+        
+
+        
+        
+event_handler = MyHandler()
+observer = Observer()
+observer.schedule(event_handler, path='/home/pi/PrevenFrontEnd', recursive=False)
+observer.start()
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    observer.stop()
+observer.join()
+
+
+
+
         
